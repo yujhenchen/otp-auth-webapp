ae9f444ad3a4344a9a30e545273c682de1fa2ded
yujhenchen <yujhen.chen93@gmail.com>
2024-06-24 22:21:13 +0800

    feat: add basic login form elements

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,4 +1,37 @@
+import { Button, Label, TextInput } from "flowbite-react";
+import { HiMail } from "react-icons/hi";
+
 export default function Login() {
-  // const onSubmit = async () => {};
-  return <div>Login</div>;
+  // const errorMessages = () => {};
+  const onSubmit = async () => {};
+
+  return (
+    <div>
+      <form>
+        <div>
+          <div>
+            <Label htmlFor="email" value="Your email" />
+          </div>
+          <TextInput
+            id="email"
+            type="email"
+            icon={HiMail}
+            placeholder="name@flowbite.com"
+            required
+          />
+        </div>
+
+        <div>
+          <div>
+            <Label htmlFor="password" value="User password" />
+          </div>
+          <TextInput id="password" type="password" required />
+        </div>
+
+        <Button type="submit" onSubmit={onSubmit}>
+          Sign In
+        </Button>
+      </form>
+    </div>
+  );
 }
