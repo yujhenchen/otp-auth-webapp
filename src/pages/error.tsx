6b9d3dd56a0711e18bd5a1ee99c5870bb2656c1b
yujhenchen <yujhen.chen93@gmail.com>
2024-06-29 21:11:47 +0800

    feat: style error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -20,11 +20,19 @@ export default function Error() {
     isRouteErrorResponse(error) ? routeError[error.status] : "Unknown error";
 
   return (
-    <div className="container relative min-h-screen mx-auto">
-      <h1>Uh-oh</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>{getRouteErrorMessage()}</p>
-      <Button type="button">Back to home page</Button>
+    <div className="container relative min-h-screen mx-auto text-center">
+      <div className="flex flex-col w-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <h1 className="text-6xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Uh-oh
+        </h1>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          {getRouteErrorMessage()}
+        </p>
+        <Button type="button">Back to home page</Button>
+      </div>
     </div>
   );
 }
